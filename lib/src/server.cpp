7646f31566688661e3407d84e1bc052f452b805f
Arnaud Boeglin <a.boeglin67@gmail.com>
2022-07-14 13:13:51 +0200

    WIP

--- lib/src/server.cpp
+++ lib/src/server.cpp
@@ -1,10 +1,35 @@
 #include "App.h"
 #include "Loop.h"
+#include "HttpResponse.h"
+
 #include "event-loop.hpp"
 #include "gc.h"
 #include "record.hpp"
-#include<thread>
+#include <thread>
 #include "uv.h"
+#include "apply-pap.hpp"
+#include "http.hpp"
+#include "list.hpp"
+#include "record.hpp"
+
+
+
+template<bool SSL> void madserver__handleResponse(uWS::HttpResponse<SSL> *res, madlib__record__Record_t *response) {
+  char *body = (char*) madlib__record__internal__selectField((char*) "body", response);
+  int64_t status = (int64_t) madlib__record__internal__selectField((char*) "status", response);
+  madlib__list__Node_t *headers = (madlib__list__Node_t*) madlib__record__internal__selectField((char*) "headers", response);
+
+  res->writeStatus(std::to_string(status));
+
+  while (headers->value) {
+    madlib__http__Header_t *header = (madlib__http__Header_t*) headers->value;
+    res->writeHeader((char*) header->name, (char*) header->value);
+
+    headers = headers->next;
+  }
+
+  res->end(body);
+}
 
 
 extern "C" {
@@ -37,6 +62,123 @@ extern "C" {
     server->uWSApp->run();
     return server;
   }
+
+  madserver__server_t *madserver__addGetHandler(char *path, PAP_t *handler, madserver__server_t *server) {
+    server->uWSApp->get(std::string(path), [handler](auto *res, auto *req) {
+      std::string bodyString;
+      res->onData([handler, res, req, bodyString = std::move(bodyString)](std::string_view data, bool last) mutable {
+        // std::cout << "data" << std::endl;
+        bodyString.append(data.data(), data.length());
+
+        if (last) {
+          // build body:
+          madlib__http__Body_t *body = (madlib__http__Body_t*) GC_MALLOC(sizeof(madlib__http__Body_t));
+          if (bodyString.empty()) {
+            body->index = 1;
+          } else {
+            body->index = 0;
+          }
+
+          madlib__list__Node_t *headers = madlib__list__empty();
+          auto headerIt = req->begin();
+          while (headerIt != req->end()) {
+            std::string_view headerName = (*headerIt).first;
+            std::string_view headerValue = (*headerIt).second;
+
+            madlib__http__Header_t *header = (madlib__http__Header_t*) GC_MALLOC(sizeof(madlib__http__Header_t));
+            header->index = 0;
+            header->name = (char*) GC_MALLOC_ATOMIC(headerName.size() + 1);
+            memcpy(header->name, headerName.data(), headerName.size());
+            header->name[headerName.size()] = '\0';
+
+            header->value = (char*) GC_MALLOC_ATOMIC(headerValue.size() + 1);
+            memcpy(header->value, headerValue.data(), headerValue.size());
+            header->value[headerValue.size()] = '\0';
+
+            headers = madlib__list__push(header, headers);
+
+            ++headerIt;
+          }
+
+
+          madlib__http__Method_t *method = (madlib__http__Method_t*) GC_MALLOC(sizeof(madlib__http__Method_t));
+          method->methodIndex = 2; // GET
+
+          char *url;
+          if (!req->getQuery().empty()) {
+            url = (char *) GC_MALLOC_ATOMIC(req->getUrl().size() + req->getQuery().size() + 2);
+            memcpy(url, req->getUrl().data(), req->getUrl().size());
+            url[req->getUrl().size()] = '?';
+            memcpy(url + req->getUrl().size() + 1, req->getQuery().data(), req->getQuery().size());
+            url[req->getUrl().size() + req->getQuery().size() + 1] = '\0';
+          } else {
+            url = (char *) GC_MALLOC_ATOMIC(req->getUrl().size() + 1);
+            memcpy(url, req->getUrl().data(), req->getUrl().size());
+            url[req->getUrl().size()] = '\0';
+          }
+
+          madlib__record__Record_t *request = (madlib__record__Record_t*) GC_MALLOC(sizeof(madlib__record__Record_t));
+
+          madlib__record__Field_t *urlField = (madlib__record__Field_t*) GC_MALLOC(sizeof(madlib__record__Field_t));
+          urlField->name = (char*) "url";
+          urlField->value = url;
+
+          madlib__record__Field_t *methodField = (madlib__record__Field_t*) GC_MALLOC(sizeof(madlib__record__Field_t));
+          methodField->name = (char*) "method";
+          methodField->value = method;
+
+          madlib__record__Field_t *bodyField = (madlib__record__Field_t*) GC_MALLOC(sizeof(madlib__record__Field_t));
+          bodyField->name = (char*) "body";
+          bodyField->value = body;
+
+          madlib__record__Field_t *headersField = (madlib__record__Field_t*) GC_MALLOC(sizeof(madlib__record__Field_t));
+          headersField->name = (char*) "headers";
+          headersField->value = headers;
+
+          madlib__record__Field_t **requestFields = (madlib__record__Field_t**) GC_MALLOC(sizeof(madlib__record__Field_t*) * 4);
+          requestFields[0] = urlField;
+          requestFields[1] = bodyField;
+          requestFields[2] = methodField;
+          requestFields[3] = headersField;
+
+          request->fieldCount = 4;
+          request->fields = requestFields;
+
+          PAP_t *callback = (PAP_t*) GC_MALLOC(sizeof(PAP_t));
+          callback->fn = (void*) madserver__handleResponse<true>;
+          callback->arity = 2;
+          callback->missingArgCount = 1;
+
+          PAPEnv_2_t *env = (PAPEnv_2_t*) GC_MALLOC(sizeof(PAPEnv_2_t));
+          env->arg0 = res;
+          callback->env = env;
+
+          __applyPAP__(handler, 2, request, callback);
+        }
+      });
+
+      res->onAborted([]() {
+        std::cout << "aborted" << std::endl;
+      });
+    });
+
+    return server;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   // void madserver__run2(void *server) {
   //   int64_t port = 3000;
@@ -79,4 +221,3 @@ extern "C" {
       }).run();
   }
 
-}
